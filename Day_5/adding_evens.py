61ad34d83c9e1aaffb609b59250e55ab3afe8359
Sayantan <sayantan.dasgupta21@gmail.com>
2021-10-18 01:28:18 +0530

    Exercises 2 and 3 added for Day 5

---
+++ Day_5/adding_evens.py
@@ -0,0 +1,38 @@
+'''
+Exercise 3 for Day 5 of 100 Days of Python
+
+For this exercise, we are going to find the sum of all the even numbers from 1 to 100, including 100
+
+So, we need to find and print the result of 
+
+2 + 4 + 6 + ... + 100
+'''
+
+'''
+We can easily carry this out using a for loop and the range() generator. Let us define a function to carry out this functionality
+'''
+
+'''
+find_even_number_sum does not take any parameters
+
+It runs a for loop between 1 to 100 and adds all the even numbers present in that range.
+We can use the range() generator with a step of 2 to get all the even numbers.
+
+So the function first initialises a total as 0
+
+Then as we access all the even numbers using the for loop, we add them one by one to the total
+'''
+def find_even_number_sum():
+
+    totalSum = 0
+
+    for evenNo in range(2,102,2):  # From the working of a range function, we know that 2 is starting value, 102 is ending value and the second 2 is the step value
+        # Quick Question: Why have we used 102 as ending value instead of 100? Think about it
+
+        totalSum += evenNo
+
+    print(f"Total Sum of the even numbers between 1 and 100, including 100 is {totalSum}")
+
+
+if __name__ == '__main__':
+    find_even_number_sum()
--- Day_5/highest_score.py
+++ Day_5/highest_score.py
@@ -6,8 +6,33 @@ In this exercise, we are going to find out the highest score from a given list o
 We are going to discuss 2 methods of finding the maximum
 '''
 
+
 def find_maximum_score(scores):
     ''' METHOD 1
-
-    
+    Using the inbuilt max() function, which takes an iterable as its parameter and returns the maximum value in the iterable
+    x = max(scores)
+    return x
     '''
+
+    '''METHOD 2
+    Declare a global max value. Compare with each and every value inside the list and update the max value accordingly
+    Use a for loop for your comparisons
+    '''
+
+    maxVal = scores[0]  # declare the first value as the maxVal
+
+    # Now using a for loop compare the maxVal with each and every item and then update if maxVal is less than one particular item
+    for i in range(1, len(scores)):
+        if maxVal < scores[i]:
+            maxVal = scores[i]
+
+    return maxVal
+
+
+if __name__ == "__main__":
+    scores = [int(score) for score in input(
+        "Enter the scores of the students in comma separated form: ").split(', ')]
+
+    maximumScore = find_maximum_score(scores)
+
+    print(f"The highest score is {maximumScore}")
